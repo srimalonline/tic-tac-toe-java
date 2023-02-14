ebb70ca433b429049924464f3d7fe41334f20177
Srimal Fernando <67434227+srimalonline@users.noreply.github.com>
2023-02-14 16:23:48 +0530

    injecting code for cli

--- tik-tack-toe-cli/src/main/java/com/mycompany/tik/tack/toe/cli/TikTackToeCli.java
+++ tik-tack-toe-cli/src/main/java/com/mycompany/tik/tack/toe/cli/TikTackToeCli.java
@@ -3,14 +3,64 @@
  */
 
 package com.mycompany.tik.tack.toe.cli;
-
+import java.util.Scanner;
 /**
  *
  * @author Srimal
  */
 public class TikTackToeCli {
+    private static char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    private static char currentPlayer = 'X';
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+
+        while (!isGameOver()) {
+            System.out.println("Current board:");
+            printBoard();
+            System.out.println("Player " + currentPlayer + ", enter your move (row[1-3] column[1-3]): ");
+
+            int row = scanner.nextInt() - 1;
+            int col = scanner.nextInt() - 1;
+
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+                board[row][col] = currentPlayer;
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+
+        System.out.println("Current board:");
+        printBoard();
+        char winner = (currentPlayer == 'X') ? 'O' : 'X';
+        System.out.println("Player " + winner + " wins!");
+    }
+
+    private static boolean isGameOver() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+                return true;
+            }
+            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+                return true;
+            }
+        }
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+            return true;
+        }
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+            return true;
+        }
+        return false;
+    }
+
+    private static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(" " + board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
+            if (i != 2) {
+                System.out.println("---|---|---");
+            }
+        }
     }
 }
