b1a72f584f4dd56849145f6631b4e5d1aa2dd042
Srimal Fernando <67434227+srimalonline@users.noreply.github.com>
2023-02-16 15:18:52 +0530

    cloning MainClass & TTT Class to the GUI app

---
+++ tik-tak-toe-gui/src/main/java/MainProgram.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TTTModel game = new TTTModel();
+
+        while (!game.checkForWin()) {
+            char[][] board = game.getBoard();
+            System.out.println("-------------");
+            System.out.println("| " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " |");
+            System.out.println("-------------");
+            System.out.println("| " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " |");
+            System.out.println("-------------");
+            System.out.println("| " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " |");
+            System.out.println("-------------");
+            System.out.println("Current player: " + game.getCurrentPlayer());
+            System.out.print("Enter row: ");
+            int row = scanner.nextInt();
+            System.out.print("Enter column: ");
+            int col = scanner.nextInt();
+            if (!game.makeMove(row, col)) {
+                System.out.println("Invalid move, try again!");
+            }
+        }
+        System.out.println("Player " + game.getCurrentPlayer() + " wins!");
+    }
+}
---
+++ tik-tak-toe-gui/src/main/java/TTTModel.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Srimal
+ */
+public class TTTModel {
+    
+    private char[][] board;
+    private char currentPlayer;
+
+    public TTTModel() {
+        board = new char[3][3];
+        currentPlayer = 'X';
+        clearBoard();
+    }
+
+    public void clearBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    public boolean makeMove(int row, int col) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+            board[row][col] = currentPlayer;
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkForWin() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+                return true;
+            }
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != ' ') {
+                return true;
+            }
+        }
+
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+            return true;
+        }
+
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
+            return true;
+        }
+
+        return false;
+    }
+}
