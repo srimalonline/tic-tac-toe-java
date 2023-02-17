13f819fab1287e93d65a0ee3caa58f3e44a1f8a4
Srimal Fernando <67434227+srimalonline@users.noreply.github.com>
2023-02-17 16:12:38 +0530

    adding multiplayer jframe

--- tik-tak-toe-gui/src/main/java/MultiPlayer.java
+++ tik-tak-toe-gui/src/main/java/MultiPlayer.java
@@ -122,6 +122,10 @@ public class MultiPlayer extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        MultiplayerTTT multi = new MultiplayerTTT();
+        multi.show();
+        
+        dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
